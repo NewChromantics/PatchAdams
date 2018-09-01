f790204af3d99301843614d05308bbf9d0404bc2
Graham Reeves <graham@grahamreeves.com>
2018-09-01 19:47:19 +0100

    Bigger plug, used in render

--- Renderer2d.js
+++ Renderer2d.js
@@ -1,16 +1,15 @@
 
-var FragShader_SdfHorse = `
+var FragShader_Hub = `
 precision highp float;
 varying vec2 uv;
 uniform sampler2D SdfTexture;
-uniform vec4 Colour;
-const vec4 Led0Colour = vec4(0,1,0,1);
-const vec4 Led1Colour = vec4(1,1,0,1);
-const vec4 LedOffColour = vec4(0.1,0.3,0.1,1);
-const int DataWidth = 8*8;
-uniform int StackIndex;
-const int StackWidth = 8;
-
+const vec3 Colour = vec3(19.0/255.0,69.0/255.0,150.0/255.0);
+const vec3 MetalColour = vec3(0.7,0.7,0.7);
+const vec3 HoleColour = vec3(0,0,0);
+const vec3 Led0Colour = vec3(0,1,0);
+const vec3 Led1Colour = vec3(1,1,0);
+const vec3 LedOffColour = vec3(0.1,0.3,0.1);
+uniform int PlugCount;
 
 float Range(float Min,float Max,float Value)
 {
@@ -23,27 +22,26 @@ bool InsideRect(vec4 Rect,vec2 uv)
 	return uv.x>=0.0 && uv.x<=1.0 && uv.y>=0.0 && uv.y<=1.0;
 }
 
-vec4 DrawPlug(vec2 uv,bool Led0,bool Led1)
+vec4 DrawPlug(vec2 uv,bool Led0Status,bool Led1Status)
 {
-	vec4 Led0Rect = vec4(0.1,0.2,0.3,0.4);
-	vec4 Led1Rect = vec4(0.7,0.2,0.9,0.4);
-	vec4 HoleRect = vec4(0.2,0.3,0.8,0.8);
-	vec4 EdgeRect = vec4(0.1,0.2,0.9,0.9);
-	if ( InsideRect(Led0Rect,uv) )  return Led0 ? Led0Colour : LedOffColour;
-	if ( InsideRect(Led1Rect,uv) )  return Led1 ? Led1Colour : LedOffColour;
-	if ( InsideRect(HoleRect,uv) )  return vec4(0,0,0,1);
-	if ( InsideRect(EdgeRect,uv) )  return vec4(0.7,0.7,0.7,1);
-	return vec4(uv,0,0);
+	vec4 SdfChannels = texture2D( SdfTexture, uv);
+	float Alpha = SdfChannels.w;
+	bool Metal = SdfChannels.r>0.5;
+	bool Led0 = SdfChannels.g>0.5;
+	bool Led1 = SdfChannels.b>0.5;
+	if ( Metal )	return vec4( MetalColour, Alpha );
+	if ( Led0 )		return vec4( Led0Status ? Led0Colour : LedOffColour, Alpha );
+	if ( Led1 )		return vec4( Led1Status ? Led1Colour : LedOffColour, Alpha );
+	return vec4( HoleColour, Alpha );
 }
 
 void main()
 {
-	gl_FragColor.xyzw = vec4( uv, 0, 1 );
-	return;
-	float Plugs=10.0;
-	float PlugIndex = uv.x*10.0;
-	float PlugU = fract(PlugIndex);
-	gl_FragColor = vec4(Colour.xyz,1);
+	gl_FragColor = vec4(Colour,1);
+	
+	float PlugIndex = uv.x * float(PlugCount);
+	
+	vec2 Pluguv = vec2( fract(PlugIndex), uv.y );
 	
 	//int Ledu = StackIndex*StackWidth+int(PlugIndex);
 	//vec2 LedUv = vec2( float(Ledu)/float(DataWidth));
@@ -52,9 +50,8 @@ void main()
 	//bool Led1 = Led0123.y > 0.5;
 	bool Led0 = true;
 	bool Led1 = true;
-	vec4 PlugColour = DrawPlug(vec2(PlugU,uv.y),Led0,Led1);
+	vec4 PlugColour = DrawPlug(Pluguv,Led0,Led1);
 	gl_FragColor.xyz = mix(gl_FragColor.xyz,PlugColour.xyz,PlugColour.w);
-	
 }
 `;
 
@@ -62,12 +59,14 @@ void main()
 
 function TRenderer2d(Context)
 {
-	this.HubShader = new TShader("Sdf", PopGlBlitter.VertShader, FragShader_SdfHorse );
+	this.HubShader = new TShader("Sdf", PopGlBlitter.VertShader, FragShader_Hub );
 	PopGlBlitter.Init(Context);
 	
 	//let SdfUrl = 'plug.png';
-	let SdfUrl = 'http://electric.horse/horse_sdf.png';
-	this.PlugSdfTexture = new TTexture("sdf",SdfUrl);
+	//let SdfUrl = 'http://electric.horse/horse_sdf.png';
+	let SdfUrl = 'https://raw.githubusercontent.com/NewChromantics/PatchAdams/master/Plug.png';
+	let LinearFilter = false;
+	this.PlugSdfTexture = new TTexture("sdf",SdfUrl,null,LinearFilter);
 	
 	
 	this.DrawScene = function(Game,RenderTarget)
@@ -85,6 +84,7 @@ function TRenderer2d(Context)
 		//console.log(Rect4);
 		let SetUniforms = function(Shader,Geo)
 		{
+			Shader.SetUniform( 'PlugCount', Hub.Plugs.length );
 			Shader.SetUniform( 'VertexRect', Rect4 );
 			Shader.SetUniform( 'SdfTexture', SdfTexture );
 		}
